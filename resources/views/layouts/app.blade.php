5c1a638b262098ba3b9c4c5524a7f613dce657cb
amir.syafiq989@gmail.com <amir.syafiq989@gmail.com>
2022-01-28 20:34:20 +0800

    update: Landing Page

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -9,7 +9,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Todo Workspace App') }}</title>
+    <title>Todo Workspace App</title>
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
